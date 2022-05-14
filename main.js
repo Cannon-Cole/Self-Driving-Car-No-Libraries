7931a4c269defc63fa8d96832ac8cce1a72a2bad
Cannon-Cole <colecannonschool@gmail.com>
2022-05-13 20:43:43 -0600

    Added lanes and utils and lerp in until

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const canvas = document.getElementById("myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
 const car = new Car(100, 100, 30, 50);
 car.draw(ctx);
 
@@ -11,6 +12,7 @@ animate();
 function animate() {
   car.update();
   canvas.height = window.innerHeight;
+  road.draw(ctx);
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
--- road.js
+++ road.js
@@ -17,14 +17,17 @@ class Road {
     ctx.lineWidth = 5;
     ctx.strokeStyle = "white";
 
-    ctx.beginPath();
-    ctx.moveTo(this.left, this.top);
-    ctx.lineTo(this.left, this.bottom);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(this.right, this.top);
-    ctx.lineTo(this.right, this.bottom);
-    ctx.stroke();
+    for (let i = 0; i <= this.laneCount; i++) {
+      const x = lerp(this.left, this.right, i / this.laneCount);
+      if (i > 0 && i < this.laneCount) {
+        ctx.setLineDash([20, 20]);
+      } else {
+        ctx.setLineDash([]);
+      }
+      ctx.beginPath();
+      ctx.moveTo(x, this.top);
+      ctx.lineTo(x, this.bottom);
+      ctx.stroke();
+    }
   }
 }
