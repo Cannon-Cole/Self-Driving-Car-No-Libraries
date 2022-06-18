37ebfc1f6624d1b7053688c61c7d4231c79c6788
Cannon-Cole <colecannonschool@gmail.com>
2022-06-17 22:03:41 -0600

    Added neural network layers

--- car.js
+++ car.js
@@ -13,6 +13,7 @@ class Car {
     this.damaged = false;
     if (controlType != "DUMMY") {
       this.sensor = new Sensor(this);
+      this.brain = new NeuralNetwork([this.sensor.rayCount, 6, 4]);
     }
     this.controls = new Controls(controlType);
   }
@@ -25,6 +26,10 @@ class Car {
     }
     if (this.sensor) {
       this.sensor.update(roadBorders, traffic);
+      const offsets = this.sensor.readings.map((s) =>
+        s == null ? 0 : 1 - s.offset
+      );
+      const outputs = NeuralNetwork.feedForward(offsets, this.brain);
     }
   }
 
